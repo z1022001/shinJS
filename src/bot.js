5881d3f0ea59f4cada2d51d2531b3a8a88d624ea
K.T.(HUA-VPC) <z1022001@gmail.com>
2020-09-23 11:17:15 +0800

    format code

--- src/bot.js
+++ src/bot.js
@@ -4,29 +4,29 @@ let LINE = new line();
 
 
 const auth = {
-	authToken: '',
-	certificate: '',
-	email: '',
-	password: ''
+    authToken: '',
+    certificate: '',
+    email: '',
+    password: ''
 }
 
-let client =  new LineConnect();
-//let client =  new LineConnect(auth);
+// let client =  new LineConnect();
+let client = new LineConnect(auth);
 
 client.startx().then(async (res) => {
-	
-	while(true) {
-		try {
-			ops = await client.fetchOps(res.operation.revision);
-		} catch(error) {
-			console.log('error',error)
-		}
-		for (let op in ops) {
-			if(ops[op].revision.toString() != -1){
-				res.operation.revision = ops[op].revision;
-				LINE.poll(ops[op])
-			}
-		}
-	}
+
+    while (true) {
+        try {
+            ops = await client.fetchOps(res.operation.revision);
+        } catch (error) {
+            console.log('error', error)
+        }
+        for (let op in ops) {
+            if (ops[op].revision.toString() != -1) {
+                res.operation.revision = ops[op].revision;
+                LINE.poll(ops[op])
+            }
+        }
+    }
 });
 
--- src/command.js
+++ src/command.js
@@ -10,8 +10,8 @@ class Command extends LineAPI {
     }
 
     get payload() {
-        if(typeof this.messages !== 'undefined'){
-            return (this.messages.text !== null) ? this.messages.text.split(' ').splice(1) : '' ;
+        if (typeof this.messages !== 'undefined') {
+            return (this.messages.text !== null) ? this.messages.text.split(' ').splice(1) : '';
         }
         return false;
     }
@@ -24,21 +24,21 @@ class Command extends LineAPI {
 
     async cancelMember() {
         let groupID;
-        if(this.payload.length > 0) {
-            let [ groups ] = await this._findGroupByName(this.payload.join(' '));
+        if (this.payload.length > 0) {
+            let [groups] = await this._findGroupByName(this.payload.join(' '));
             groupID = groups.id;
-        } 
+        }
         let gid = groupID || this.messages.to;
         let { listPendingInvite } = await this.searchGroup(gid);
-        if(listPendingInvite.length > 0){
-            this._cancel(gid,listPendingInvite);
+        if (listPendingInvite.length > 0) {
+            this._cancel(gid, listPendingInvite);
         }
     }
 
     async searchGroup(gid) {
         let listPendingInvite = [];
         let thisgroup = await this._getGroups([gid]);
-        if(thisgroup[0].invitee !== null) {
+        if (thisgroup[0].invitee !== null) {
             listPendingInvite = thisgroup[0].invitee.map((key) => {
                 return key.mid;
             });
@@ -47,21 +47,21 @@ class Command extends LineAPI {
             return { mid: key.mid, dn: key.displayName };
         });
 
-        return { 
+        return {
             listMember,
             listPendingInvite
         }
     }
 
     OnOff() {
-        if(this.isAdminOrBot(this.messages._from)){
-            let [ actions , status ] = this.messages.text.split(' ');
+        if (this.isAdminOrBot(this.messages._from)) {
+            let [actions, status] = this.messages.text.split(' ');
             const action = actions.toLowerCase();
             const state = status.toLowerCase() == 'on' ? 1 : 0;
             this.stateStatus[action] = state;
-            this._sendMessage(this.messages,`Status: \n${JSON.stringify(this.stateStatus)}`);
+            this._sendMessage(this.messages, `Status: \n${JSON.stringify(this.stateStatus)}`);
         } else {
-            this._sendMessage(this.messages,`You Are Not Admin`);
+            this._sendMessage(this.messages, `You Are Not Admin`);
         }
     }
 
@@ -69,19 +69,19 @@ class Command extends LineAPI {
         let mentionStrings = [''];
         let mid = [''];
         for (var i = 0; i < listMember.length; i++) {
-            mentionStrings.push('@'+listMember[i].displayName+'\n');
+            mentionStrings.push('@' + listMember[i].displayName + '\n');
             mid.push(listMember[i].mid);
         }
         let strings = mentionStrings.join('');
         let member = strings.split('@').slice(1);
-        
+
         let tmp = 0;
         let memberStart = [];
-        let mentionMember = member.map((v,k) => {
+        let mentionMember = member.map((v, k) => {
             let z = tmp += v.length + 1;
             let end = z - 1;
             memberStart.push(end);
-            let mentionz = `{"S":"${(isNaN(memberStart[k - 1] + 1) ? 0 : memberStart[k - 1] + 1 ) }","E":"${end}","M":"${mid[k + 1]}"}`;
+            let mentionz = `{"S":"${(isNaN(memberStart[k - 1] + 1) ? 0 : memberStart[k - 1] + 1)}","E":"${end}","M":"${mid[k + 1]}"}`;
             return mentionz;
         })
         return {
@@ -98,30 +98,30 @@ class Command extends LineAPI {
         }
         return;
     }
-    
-    async recheck(cs,group) {
+
+    async recheck(cs, group) {
         let users;
         for (var i = 0; i < cs.length; i++) {
-            if(cs[i].group == group) {
+            if (cs[i].group == group) {
                 users = cs[i].users;
             }
         }
-        
+
         let contactMember = await this._getContacts(users);
         return contactMember.map((z) => {
-                return { displayName: z.displayName, mid: z.mid };
-            });
+            return { displayName: z.displayName, mid: z.mid };
+        });
     }
 
     removeReaderByGroup(groupID) {
         const groupIndex = this.checkReader.findIndex(v => {
-            if(v.group == groupID) {
+            if (v.group == groupID) {
                 return v
             }
         })
 
-        if(groupIndex != -1) {
-            this.checkReader.splice(groupIndex,1);
+        if (groupIndex != -1) {
+            this.checkReader.splice(groupIndex, 1);
         }
     }
 
@@ -134,12 +134,12 @@ class Command extends LineAPI {
     }
 
     vn() {
-        this._sendFile(this.messages,`${__dirname}/../download/${this.payload.join(' ')}.m4a`,3);
+        this._sendFile(this.messages, `${__dirname}/../download/${this.payload.join(' ')}.m4a`, 3);
     }
 
     checkKernel() {
-        exec('uname -a',(err, sto) => {
-            if(err) {
+        exec('uname -a', (err, sto) => {
+            if (err) {
                 this._sendMessage(this.messages, err);
                 return
             }
@@ -162,17 +162,19 @@ class Command extends LineAPI {
 
     creator() {
         let msg = {
-            text:null,
+            text: null,
             contentType: 13,
             contentPreview: null,
-            contentMetadata: 
-            { mid: 'uc903926f2160feb965fbb3144da8f490',
-            displayName: 'Aka Shinobi' }
+            contentMetadata:
+            {
+                mid: 'uc903926f2160feb965fbb3144da8f490',
+                displayName: 'Aka Shinobi'
+            }
         }
-        Object.assign(this.messages,msg);
+        Object.assign(this.messages, msg);
         this._sendMessage(this.messages);
     }
-    
+
     resetStateUpload() {
         this.stateUpload = {
             file: '',
@@ -189,15 +191,15 @@ class Command extends LineAPI {
             group: this.messages.to,
             sender: this.messages._from
         };
-        this._sendMessage(this.messages,`select pict/video for upload ${this.stateUpload.name}`);
+        this._sendMessage(this.messages, `select pict/video for upload ${this.stateUpload.name}`);
         return;
     }
-    
+
     async doUpload({ id, contentType }) {
         let url = `https://obs-sg.line-apps.com/talk/m/download.nhn?oid=${id}`;
-        await this._download(url,this.stateUpload.name, contentType);
+        await this._download(url, this.stateUpload.name, contentType);
         this.messages.contentType = 0;
-        this._sendMessage(this.messages,`Upload ${this.stateUpload.name} success !!`);
+        this._sendMessage(this.messages, `Upload ${this.stateUpload.name} success !!`);
         this.resetStateUpload()
         return;
     }
@@ -206,30 +208,30 @@ class Command extends LineAPI {
         let name = this.payload.join(' ');
         let dirName = `${__dirname}/../download/${name}.jpg`;
         try {
-            this._sendImage(this.messages,dirName);
+            this._sendImage(this.messages, dirName);
         } catch (error) {
-             this._sendImage(this.messages,`No Photo #${name} Uploaded `);
+            this._sendImage(this.messages, `No Photo #${name} Uploaded `);
         }
-        return ;
-        
+        return;
+
     }
 
     async joinQr() {
-        const [ ticketId ] = this.payload[0].split('g/').splice(-1);
+        const [ticketId] = this.payload[0].split('g/').splice(-1);
         let { id } = await this._findGroupByTicket(ticketId);
-        await this._acceptGroupInvitationByTicket(id,ticketId);
+        await this._acceptGroupInvitationByTicket(id, ticketId);
         return;
     }
 
     async qrOpenClose() {
         let updateGroup = await this._getGroup(this.messages.to);
         updateGroup.preventedJoinByTicket = true;
-        if(typeof this.payload !== 'undefined') {
-            let [ type ] = this.payload;
-            if(type === 'open') {
+        if (typeof this.payload !== 'undefined') {
+            let [type] = this.payload;
+            if (type === 'open') {
                 updateGroup.preventedJoinByTicket = false;
                 const groupUrl = await this._reissueGroupTicket(this.messages.to)
-                this._sendMessage(this.messages,`Line group = line://ti/g/${groupUrl}`);
+                this._sendMessage(this.messages, `Line group = line://ti/g/${groupUrl}`);
             }
         }
         await this._updateGroup(updateGroup);
@@ -237,15 +239,15 @@ class Command extends LineAPI {
     }
 
     spamGroup() {
-        if(this.isAdminOrBot(this.messages._from) && this.payload[0] !== 'kill') {
+        if (this.isAdminOrBot(this.messages._from) && this.payload[0] !== 'kill') {
             let s = [];
             for (let i = 0; i < this.payload[1]; i++) {
                 let name = `${Math.ceil(Math.random() * 1000)}${i}`;
                 this.spamName.push(name);
-                this._createGroup(name,[this.payload[0]]);
+                this._createGroup(name, [this.payload[0]]);
             }
             return;
-        } 
+        }
         for (let z = 0; z < this.spamName.length; z++) {
             this.leftGroupByName(this.spamName[z]);
         }
@@ -253,82 +255,82 @@ class Command extends LineAPI {
     }
 
     checkIP() {
-        exec(`wget ipinfo.io/${this.payload[0]} -qO -`,(err, res) => {
-            if(err) {
-                this._sendMessage(this.messages,'Error Please Install Wget');
-                return 
+        exec(`wget ipinfo.io/${this.payload[0]} -qO -`, (err, res) => {
+            if (err) {
+                this._sendMessage(this.messages, 'Error Please Install Wget');
+                return
             }
             const result = JSON.parse(res);
-            if(typeof result.error == 'undefined') {
+            if (typeof result.error == 'undefined') {
                 const { org, country, loc, city, region } = result;
                 try {
-                    const [latitude, longitude ] = loc.split(',');
+                    const [latitude, longitude] = loc.split(',');
                     let location = new Location();
-                    Object.assign(location,{ 
+                    Object.assign(location, {
                         title: `Location:`,
                         address: `${org} ${city} [ ${region} ]\n${this.payload[0]}`,
                         latitude: latitude,
                         longitude: longitude,
-                        phone: null 
+                        phone: null
                     })
-                    const Obj = { 
+                    const Obj = {
                         text: 'Location',
-                        location : location,
+                        location: location,
                         contentType: 0,
                     }
-                    Object.assign(this.messages,Obj)
-                    this._sendMessage(this.messages,'Location');
+                    Object.assign(this.messages, Obj)
+                    this._sendMessage(this.messages, 'Location');
                 } catch (err) {
-                    this._sendMessage(this.messages,'Not Found');
+                    this._sendMessage(this.messages, 'Not Found');
                 }
             } else {
-                this._sendMessage(this.messages,'Location Not Found , Maybe di dalem goa');
+                this._sendMessage(this.messages, 'Location Not Found , Maybe di dalem goa');
             }
         })
         return;
     }
 
     async rechecks() {
-        let rec = await this.recheck(this.checkReader,this.messages.to);
+        let rec = await this.recheck(this.checkReader, this.messages.to);
         const mentions = await this.mention(rec);
         this.messages.contentMetadata = mentions.cmddata;
-        await this._sendMessage(this.messages,mentions.names.join(''));
+        await this._sendMessage(this.messages, mentions.names.join(''));
         return;
     }
 
     async kickAll() {
         let groupID;
-        if(this.stateStatus.kick == 1 && this.isAdminOrBot(this.messages._from)) {
+        if (this.stateStatus.kick == 1 && this.isAdminOrBot(this.messages._from)) {
             let target = this.messages.to;
-            if(this.payload.length > 0) {
-                let [ groups ] = await this._findGroupByName(this.payload.join(' '));
+            if (this.payload.length > 0) {
+                let [groups] = await this._findGroupByName(this.payload.join(' '));
                 groupID = groups.id;
             }
             let { listMember } = await this.searchGroup(groupID || target);
             for (var i = 0; i < listMember.length; i++) {
-                if(!this.isAdminOrBot(listMember[i].mid)){
-                    this._kickMember(groupID || target,[listMember[i].mid])
+                if (!this.isAdminOrBot(listMember[i].mid)) {
+                    this._kickMember(groupID || target, [listMember[i].mid])
                 }
             }
             return;
-        } 
+        }
         return this._sendMessage(this.messages, ' Kick Failed check status or admin only !');
     }
 
     async checkIG() {
         try {
             let { userProfile, userName, bio, media, follow } = await this._searchInstagram(this.payload[0]);
-            await this._sendFileByUrl(this.messages,userProfile);
+            await this._sendFileByUrl(this.messages, userProfile);
             await this._sendMessage(this.messages, `${userName}\n\nBIO:\n${bio}\n\n\uDBC0 ${follow} \uDBC0`)
-            if(Array.isArray(media)) {
+            if (Array.isArray(media)) {
                 for (let i = 0; i < media.length; i++) {
-                    await this._sendFileByUrl(this.messages,media[i]);
+                    await this._sendFileByUrl(this.messages, media[i]);
                 }
             } else {
-                this._sendMessage(this.messages,media);
+                this._sendMessage(this.messages, media);
             }
         } catch (error) {
-            this._sendMessage(this.messages,`Error: ${error}`);
+            this._sendMessage(this.messages, `Error: ${error}`);
         }
         return;
     }
